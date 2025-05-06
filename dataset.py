9373bde7783d5396d41ae1109a3183f1a4c494f0
Eddy Ogola <eddy.ogola1@gmail.com>
2025-05-06 10:04:13 -0400

    run dataset class in notebook

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from PIL import Image
 import json
+import tqdm
 import os
 
 
@@ -9,7 +10,7 @@ class ZindDataset(Dataset):
         self.data_path = data_path
         self.data = []
         
-        for i in range(size):
+        for i in tqdm.tqdm(range(size)):
             dir_path = os.path.join(data_path, f"{i:04d}")
 
             floor_plans = []
@@ -23,12 +24,12 @@ class ZindDataset(Dataset):
             with open(room_layout_json_path, "r") as f:
                 room_layout = json.load(f)
             
-            for floor_plan_file in os.listdir(floor_plans_path):
+            for floor_plan_file in tqdm.tqdm(os.listdir(floor_plans_path)):
                 floor_plan_path = os.path.join(floor_plans_path, floor_plan_file)
                 floor_plan_img = Image.open(floor_plan_path)
                 floor_plans.append(floor_plan_img)
 
-            for pano_file in os.listdir(panos_path):
+            for pano_file in tqdm.tqdm(os.listdir(panos_path)):
                 pano_path = os.path.join(panos_path, pano_file)
                 pano_img = Image.open(pano_path)
                 panos.append(pano_img)
@@ -50,3 +51,7 @@ class ZindDataset(Dataset):
 if __name__ == "__main__":
     zind_path = "../data/zillow/zind/data"
     dataset = ZindDataset(zind_path)
+    print("Dataset size:", len(dataset))
+
+    item = dataset[0]
+    print(item)
